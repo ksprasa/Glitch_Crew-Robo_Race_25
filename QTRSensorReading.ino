1f1cd14b3a7e6a263b911e3cc04730d8cd55afee
ksprasa <prasannaks2007@gmail.com>
2025-09-20 20:24:01 +0530

    Add files via upload

---
+++ QTRSensorReading.ino
@@ -0,0 +1,24 @@
+#include <QTRSensors.h>
+
+#define NUM_SENSORS 8
+#define TIMEOUT 2500
+#define EMITTERPIN 2
+
+QTRSensorsRC qtrrc((unsigned char[]){3,4,5,6,7,8,9,10}, NUM_SENSORS, TIMEOUT, EMITTERPIN);
+
+unsigned int sensorVals[NUM_SENSORS];
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop() {
+  qtrrc.read(sensorVals);
+  for (uint8_t i = 0; i < NUM_SENSORS; i++ ) {
+    Serial.print(sensorVals[i]);
+    Serial.print('\t');
+  }
+  Serial.println();
+  delay(200);
+}
